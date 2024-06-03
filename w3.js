47402032c7dfc00db3261c559a4277ce60137e35
Vasu Verma <155059082+Vasuverma5959@users.noreply.github.com>
2024-06-03 07:48:07 +0530

    Add files via upload

---
+++ w3.js
@@ -0,0 +1,30 @@
+
+// function menu(){
+//     var a = document.getElementsByClassName("Tutorial")
+//     // a.style.display = "flex"
+// }
+// function open(){
+//     // a.classList.add(".Tutorial")
+//     var a = document.getElementById("menu_bar").classList.add('Tutorial')
+
+// }
+// function open(){
+//     var a = document.getElementById("Tutorial1")
+//     a.style.display="flex"
+// }
+
+// let a = document.getElementsByClassName("Tutorial")
+// function openPopup(){
+//     a.classList.add(".popup")
+
+// }
+
+// let a = document.getElementById("Tutorial")
+// a.addEventListener('click',function(){
+//     let b = a.classList.add('open-popup')
+
+// })
+function open22() {
+    var a = document.getElementById("Tutorial1")
+    a.style.display="flex"
+}
